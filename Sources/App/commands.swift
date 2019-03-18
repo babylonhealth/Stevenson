92569151b7da49c6835fa25336295a2e5fae2a52
Ilya Puchka <ilya.puchka@babylonhealth.com>
2019-03-18 21:23:37 +0000

    method for extracting branch form options

--- Sources/App/commands.swift
+++ Sources/App/commands.swift
@@ -18,16 +18,19 @@ enum Commands {
             let options = components.dropFirst().joined(separator: " ")
             var args = ["FASTLANE": lane, "OPTIONS": options]
 
-            let branchArgument = CircleCIService.branchArgument
-            let branchOptionPrefix = "\(branchArgument):"
-            let branch = components.dropFirst()
-                .first { $0.hasPrefix(branchOptionPrefix) }?
-                .dropFirst(branchOptionPrefix.count)
-
-            if let branch = branch {
-                args[branchArgument] = String(branch)
+            if let branch = Commands.branch(fromOptions: components) {
+                args[CircleCIService.branchArgument] = branch
             }
 
             return Command(name: lane, arguments: args)
     })
+
+    private static func branch(fromOptions options: [String]) -> String? {
+        let branchArgument = CircleCIService.branchArgument
+        let branchOptionPrefix = "\(branchArgument):"
+        let branch = options.dropFirst()
+            .first { $0.hasPrefix(branchOptionPrefix) }?
+            .dropFirst(branchOptionPrefix.count)
+        return branch.map(String.init)
+    }
 }
