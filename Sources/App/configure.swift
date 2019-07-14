23877356fe45cd5c13667209fba3ace68d66276c
Olivier Halligon <olivier.halligon@babylonhealth.com>
2019-07-14 18:21:56 +0200

    Configure known/whitelisted projects

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,8 +2,24 @@ import Vapor
 import Stevenson
 
 private let jiraProjects = [
-    // TODO: Provide list of all the projects on which Stevenson is allowed to create versions
-    "FCTP": 17165
+    "NRX"  : 16911, // Enrolment and Integrity
+    "CE"   : 16937, // Consultation Experience
+    "AV"   : 16942, // Core Experience / Avalon
+    "CW"   : 16832, // Consumer Web
+    "CNSMR": 16968, // Consumer Apps (Native/Core)
+    "MON"  : 10103, // HealthCheck
+    "GW"   : 16949, // Triage UI
+    "PRSCR": 16840, // Prescriptions
+    "SDK"  : 16975, // SDK
+    "APPTS": 16875, // Booking/Appointments
+    "TEL"  : 16857, // Telus
+    "PRO"  : 16980, // Professional Services
+    "PAR"  : 17098, // Partnerships
+    "MN"   : 17031, // Monitor
+    "TES"  : 17074, // Tests & Kits
+    "WH"   : 17112, // Women's Health
+    "IDM"  : 16903, // Identity Platform / Identity Management
+    "CM"   : 16920, // Condition Management
 ]
 
 /// Called before your application initializes.
