0cdf65423f25e1e98ff58269189ad21984ba997c
Ilya Puchka <ilyapuchka@gmail.com>
2019-10-15 14:54:52 +0100

    Add back individual commands

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -53,7 +53,11 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     let router = EngineRouter.default()
     try routes(router: router, slack: slack, commands: [
-        .stevenson(ci, jira, github)
+        .stevenson(ci, jira, github),
+        .fastlane(ci),
+        .appcenter(ci),
+        .testflight(ci),
+        .crp(jira, github)
     ])
     services.register(router, as: Router.self)
 
