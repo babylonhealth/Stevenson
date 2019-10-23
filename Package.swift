471030f6894bb0b90ae91d4cdc57625574e7fc2c
Olivier Halligon <olivier.halligon@babylonhealth.com>
2019-10-23 19:02:54 +0200

    Bump to Swift 5.1
    @propertyWrapper were introduced in Swift 5.1

--- Package.swift
+++ Package.swift
@@ -11,6 +11,6 @@ let package = Package(
         .target(name: "App", dependencies: ["Stevenson"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
-
