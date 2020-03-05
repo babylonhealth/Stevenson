fe81175bc56f94ce4d50e20ec2fd3066c27cd898
Mark Bates <55098036+markbatesBHrm@users.noreply.github.com>
2020-03-05 10:43:31 +0000

    [IOSP-599] Whitelist multiple new JIRA projects (#50)
    * Added AND,IDP,COCO,ETA,& ETR projects to whitelist
    
    * Additional project added - ACP
    
    Co-authored-by: Olivier Halligon <olivier@halligon.net>

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,16 +3,22 @@ import Stevenson
 
 // Use https://babylonpartners.atlassian.net/rest/api/3/project/<ProjectKey> to get the corresponding ID
 private let jiraProjects = [
+    "ACP"   : 17420, // Assistant Chat Platform
+    "AND"   : 13700, // Android Engineering
     "ANDRP" : 17264, // Android Platform
     "APPTS" : 16875, // Booking/Appointments
     "AV"    : 16942, // Onboarding and Navigation (ex Core Experience / Avalon)
     "CE"    : 16937, // Consultation Experience
     "CNSMR" : 16968, // Consumer Apps (Native/Core)
+    "COCO"  : 17344, // Continuous Compliance
     "COREUS": 17127, // Babylon US Core Product
     "CW"    : 16832, // Consumer Web
+    "ETA"   : 17369, // Engagement Tribe - Activate
+    "ETR"   : 17372, // Engagement Tribe - Retain
     "GW"    : 16949, // Triage UI
     // TODO: [IOSP-101/IOSP-147] Re-enable IDM once this board have migrated away from NextGen board
     // "IDM"   : 16903, // Identity Platform / Identity Management
+    "IDP"   : 17228, // Identity Access Management
     "IOSP"  : 17263, // iOS Native Apps Platform
     "MS"    : 17233, // Monitor
     "MON"   : 10103, // HealthCheck
