1086ce5e39a094d8479ad6a293528bdd33e88910
markbatesBHrm <mark.bates@babylonhealth.com>
2020-05-26 14:44:52 +0100

    Added PDT project to whitelist

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -28,6 +28,7 @@ private let jiraProjects = [
     "NRX"   : 16911, // GP@Hand Registrations (ex Enrolment and Integrity)
     "PAR"   : 17098, // Partnerships
     "PAYE"  : 17510, // Payments & Eligibility
+    "PDT"   : 17501, // Partner Development
     "PRO"   : 16980, // Professional Services
     "PRSCR" : 16840, // Prescriptions
     "REFER" : 17244, // Referrals (next-gen)
