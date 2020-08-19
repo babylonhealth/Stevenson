42556b35d53143b579048c6c053e50383be745da
markbatesBHrm <mark.bates@babylonhealth.com>
2020-08-19 14:47:03 +0100

    Revert 4153855

--- Sources/App/CRP/JiraService+CRP.swift
+++ Sources/App/CRP/JiraService+CRP.swift
@@ -210,15 +210,15 @@ extension JiraService {
             self.changeScope = FieldType.TextArea.Document(text: "There are a number of tickets from the Changelog that are yet to be moved to a completed status or resolution in their respective workflow. Each of these have been reviewed and commented on with why they do not impact the release, yet are in the codebase. These tickets are:")
             self.jiraReleaseURL = "\(jiraBaseURL)/secure/Dashboard.jspa?selectPageId=15452"
             self.githubReleaseURL = "https://github.com/\(release.repository.fullName)/releases/tag/\(release.appName)/\(release.version)"
-            self.testing = FieldType.TextArea.Document(text: "Test Plan - [Android & iOS native mobile apps PED](https://docs.google.com/document/d/1GlvBD7DL0B24WOdky_sCJp3bewmwuHHkYfYcrPWDQEI/edit#heading=h.1jdzrbj14q2r)"
-            "TestRail milestone (automated & manual test runs):"
-            "CircleCI branch pipeline (automated unit tests and build):"
-            "Internal release notes/QA sign-off"):")
+            self.testing = FieldType.TextArea.Document(text: "Test Plan - [Android & iOS native mobile apps PED](https://docs.google.com/document/d/1GlvBD7DL0B24WOdky_sCJp3bewmwuHHkYfYcrPWDQEI/edit#heading=h.1jdzrbj14q2r)
+            TestRail milestone (automated & manual test runs)
+            CircleCI branch pipeline (automated unit tests and build)
+            Internal release notes/QA sign-off")
             self.accountablePerson = accountablePerson
             self.infoSecChecked = .no
-            self.serviceChanges = FieldType.TextArea.Document(text: "Product Changes:"
-            "Service Changes:"
-            "BOM:")
+            self.serviceChanges = FieldType.TextArea.Document(text: "Product Changes
+            Service Changes
+            BOM")
             self.clinicalApproval = .unapproved
             self.regulatoryApproval = .unapproved
         }
