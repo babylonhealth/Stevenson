49f809d40f897742a4b6934fe39c114c7f9225b9
Daniel Spindelbauer <dan.spindelbauer@babylonhealth.com>
2020-08-19 15:12:02 +0100

    [PDT-1399] Update CRP ticket with new required fields (#66)
    * Added xcode version
    
    * - Added Clinical and Regulatory Approval field types
    - Renamed `businessImpact` to `changeScope`
    - Added `serviceChanges`, `clinicalApproval`, `regulatoryApproval` fields
    - Use a json file instead of multiline string for testing
    
    * Include mock response for test target
    
    * Use Swift 5.3
    
    * There’s no Swift 5.3. yet :see-no-evil:
    
    This reverts commit 655c7c8b7bb23a12c1d20c102997176d5449323c.
    
    * Use process instead of copy?
    
    * Revert back to using multiline string until Swift 5.3 is released
    
    * Update changeSCope
    
    Co-authored-by: Mark Bates <55098036+markbatesBHrm@users.noreply.github.com>
    
    * Added pre-propulated values
    
    * Update tests
    
    Co-authored-by: Mark Bates <55098036+markbatesBHrm@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -10,7 +10,10 @@ let package = Package(
         .target(name: "Stevenson", dependencies: ["Vapor"]),
         .target(name: "App", dependencies: ["Stevenson"]),
         .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App"])
+        .testTarget(
+            name: "AppTests",
+            dependencies: ["App"]
+        )
     ],
     swiftLanguageVersions: [.v5]
 )
--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -470,11 +470,32 @@ extension AppTests {
               "version" : 1
             },
             "customfield_11514" : "2019-10-31",
+            "customfield_12763" : {
+              "id" : "12569"
+            },
             "issuetype" : {
               "id" : "11439"
             },
             "project" : {
               "id" : "12345"
+            },
+            "customfield_12762" : {
+              "id" : "12566"
+            },
+            "customfield_13350" : {
+              "type" : "doc",
+              "content" : [
+                {
+                  "type" : "paragraph",
+                  "content" : [
+                    {
+                      "type" : "text",
+                      "text" : "Product Changes:\nService Changes:\nBOM:"
+                    }
+                  ]
+                }
+              ],
+              "version" : 1
             },
             "customfield_12541" : "https:\/\/github.com\/company\/project\/releases\/tag\/app\/1.2.3",
             "customfield_11512" : {
@@ -485,7 +506,7 @@ extension AppTests {
                   "content" : [
                     {
                       "type" : "text",
-                      "text" : "TBD"
+                      "text" : "Test Plan - [Android & iOS native mobile apps PED](https://docs.google.com/document/d/1GlvBD7DL0B24WOdky_sCJp3bewmwuHHkYfYcrPWDQEI/edit)\nTestRail milestone (automated & manual test runs) -\nCircleCI branch pipeline (automated unit tests and build) -\nInternal release notes/QA sign-off -"
                     }
                   ]
                 }
@@ -516,7 +537,7 @@ extension AppTests {
                   "content" : [
                     {
                       "type" : "text",
-                      "text" : "TBD"
+                      "text" : "There are a number of tickets from the Changelog that are yet to be moved to a completed status or resolution in their respective workflow. Each of these have been reviewed and commented on with why they do not impact the release, yet are in the codebase. These tickets are:"
                     }
                   ]
                 }
